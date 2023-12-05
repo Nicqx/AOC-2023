9c5e1daa3ecc8916835e3d878bdbbbc853fc4a92
nicqx <miklos.d.parrag@gmail.com>
2023-12-05 10:04:46 +0100

    day 3

--- Days/Day3.py
+++ Days/Day3.py
@@ -5,16 +5,11 @@ from Utilities.read_file_to_string_array import read_to_array
 
 class Day3:
     arr = []
-<<<<<<< HEAD
     points = []
-=======
-    mat = []
->>>>>>> origin/main
 
     def __init__(self, file):
         self.arr.clear()
         self.arr = read_to_array(file)
-<<<<<<< HEAD
 
     def task1(self):
         result = 0
@@ -31,12 +26,6 @@ class Day3:
         mo = re.findall(r'\d+', validated_numbers)
         for item in mo:
             result += int(item)
-=======
-        self.mat = self.convert_array_to_matrix(self.arr)
-
-    def task1(self):
-        result = 0
->>>>>>> origin/main
 
         return str(result)
 
@@ -45,7 +34,6 @@ class Day3:
 
         return str(result)
 
-<<<<<<< HEAD
     def activate_number(self, x, y):
         if self.get_point(x, y).get_keep() and self.get_point(x, y).get_type() == "number":
             if x > 0:
@@ -178,15 +166,3 @@ class Point:
 
     def set_keep(self):
         self.keep = True
-=======
-    @staticmethod
-    def convert_array_to_matrix(arr):
-        result = []
-        for item in arr:
-            result.append(list(item))
-        return result
-
-    class Point:
-        type = "" # point, number, symbol
-        checked = False
->>>>>>> origin/main
--- Tests/test_Day3.py
+++ Tests/test_Day3.py
@@ -3,19 +3,11 @@ from unittest import TestCase
 from Days.Day3 import Day3
 
 
-<<<<<<< HEAD
-class TestDay3(TestCase):
-
-    def test_task1(self):
-        expected_value = str(4361)
-        actual_value = Day3('../Resources/Day3/test').task1()
-=======
 class TestDay1(TestCase):
 
     def test_task1(self):
         expected_value = str(4361)
-        actual_value = Day3('../Resources/Day3/test_s').task1()
->>>>>>> origin/main
+        actual_value = Day3('../Resources/Day3/test').task1()
         self.assertEqual(expected_value, actual_value)
 
     # def test_task2(self):
@@ -24,7 +16,7 @@ class TestDay1(TestCase):
     #     self.assertEqual(expected_value, actual_value)
 
     # def test_original(self):
-<<<<<<< HEAD
+
     #     expected_value_task1 = str(554003)
     #     expected_value_task2 = str(54649)
     #     d3 = Day3('../Resources/Day3/input')
@@ -32,12 +24,3 @@ class TestDay1(TestCase):
     #     self.assertEqual(expected_value_task1, actual_value_task1)
     #     actual_value_task2 = d3.task2()
     #     self.assertEqual(expected_value_task2, actual_value_task2)
-=======
-    #     expected_value_task1 = str(54081)
-    #     expected_value_task2 = str(54649)
-    #     d1 = Day3('../Resources/Day3/input')
-    #     actual_value_task1 = d1.task1()
-    #     self.assertEqual(expected_value_task1, actual_value_task1)
-    #     actual_value_task2 = d1.task2()
-    #     self.assertEqual(expected_value_task2, actual_value_task2)
->>>>>>> origin/main
